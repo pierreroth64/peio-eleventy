983e9d78ec7bc0a96d7a083a337975568c82c73c
Peio Roth <pierreroth64@gmail.com>
2021-12-25 22:18:36 +0100

    fix tailwind css warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: {
-    content: [
-      "src/**/*.html",
-      "src/**/*.njk",
-      "src/**/*.md"
-    ],
-    options: {
-      whitelist: [],
-    },
-  },
+  content: [
+    "src/**/*.html",
+    "src/**/*.njk",
+    "src/**/*.md"
+  ],
   theme: {},
   variants: {},
   plugins: [],
