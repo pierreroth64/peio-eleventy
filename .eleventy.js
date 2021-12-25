29be0fc5482de9bbecc609cededf13b1c47635a4
Peio Roth <pierreroth64@gmail.com>
2021-12-25 21:54:06 +0100

    conf cleanups

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 const { DateTime } = require("luxon");
-const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
@@ -7,27 +6,22 @@ const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 
 module.exports = function(eleventyConfig) {
-  // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
 
-  // https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
 
-  // Alias `layout: post` to `layout: layouts/post.njk`
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
   eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
   });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
   });
 
-  // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
     if(!Array.isArray(array) || array.length === 0) {
       return [];
@@ -39,7 +33,6 @@ module.exports = function(eleventyConfig) {
     return array.slice(0, n);
   });
 
-  // Return the smallest number argument
   eleventyConfig.addFilter("min", (...numbers) => {
     return Math.min.apply(null, numbers);
   });
@@ -50,7 +43,6 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("filterTagList", filterTagList)
 
-  // Create an array of all tags
   eleventyConfig.addCollection("tagList", function(collection) {
     let tagSet = new Set();
     collection.getAll().forEach(item => {
@@ -60,13 +52,9 @@ module.exports = function(eleventyConfig) {
     return filterTagList([...tagSet]);
   });
 
-  // As our CSS is generated in to a folder that git ignores, we want to make sure eleventy still reads it
   eleventyConfig.setUseGitIgnore(false);
-
-  // Watch our generated CSS file for changes
   eleventyConfig.addWatchTarget("./.tmp/css/style.css");
 
-  // Passthrough additional files and folders
   eleventyConfig.addPassthroughCopy({ "./.tmp/css/style.css": "static/css/style.css" });
   eleventyConfig.addPassthroughCopy({ "./src/site/_includes/fonts": "static/fonts" });
   eleventyConfig.addPassthroughCopy({ "./src/site/_includes/img": "static/img" });
